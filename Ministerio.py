f020ff01eca063a2fb09d7dc1b44c19583f4bf4e
Juan Manuel Fernandez <juanmanuelunlu@gmail.com>
2021-07-15 11:28:45 -0300

    Se mejoran los scripts

--- Ministerio.py
+++ Ministerio.py
@@ -5,49 +5,70 @@ Created on Wed Jul 14 12:10:56 2021
 @author: Juan
 """
 
-DIRECTORIO = 'C:/Users/Juan/Desktop/'
+def main(DIRECTORIO, FOLDER, legajo, titulo_araucano, sede_araucano, promedio_con_aplazos, promedio_sin_aplazos):
 
-##########################################################################
-######################### DATOS DEL ESTUDIANTE ###########################
+    # Se importan las funciones propias para querys y generación de txts
+    from functions_min import query_ministerio_academicos, query_ministerio_personales, query_ministerio_analitico, generar_textfile
+    
+    # Se importan las librerías para conectar a PostgreSQL y la creación de carpetas
+    import psycopg2
+    import os
 
-APELLIDO = 'DIAZ'
-LEGAJO = 117260
-TITULO_ARAUCANO = 442
-SEDE_ARAUCANO = 292
-PROMEDIO_APLAZOS = 8.51
-PROMEDIO_SIN_APLAZOS = 8.51
+    # Se genera una carpeta con el nombre ingresado en el directorio definido para
+    # la creación de los archivos de salida
+    DIRECTORIO_TXTS = DIRECTORIO + FOLDER + '/'
+    try:
+        os.mkdir(DIRECTORIO_TXTS)
+        put_text('Se crea el directorio...').style('color: red')
+    except:
+        put_text('El directorio ya estaba creado...').style('color: red')
 
-##########################################################################
-##########################################################################
+    # Se genera la conexión a la db postgresql
+    conn = psycopg2.connect(host="localhost", port = 5432, database="exportaciones_unlu", user="postgres", password="888888")
 
-from functions_min import query_ministerio_academicos, query_ministerio_personales, query_ministerio_analitico, generar_textfile
-import psycopg2
-import os
+    # Se transcriben los querys con los parámetros
+    query_academicos = query_ministerio_academicos(legajo, titulo_araucano, sede_araucano, promedio_con_aplazos, promedio_sin_aplazos)
+    query_personales = query_ministerio_personales(legajo)
+    query_analitico = query_ministerio_analitico(legajo)
 
-# Se genera una carpeta con el apellido
-DIRECTORIO_TXTS = DIRECTORIO + APELLIDO + '/'
+    # Se generan los txt
+    generar_textfile(conn, query_academicos, DIRECTORIO_TXTS+'academicos.txt')
+    generar_textfile(conn, query_personales, DIRECTORIO_TXTS+'personales.txt')
+    generar_textfile(conn, query_analitico, DIRECTORIO_TXTS+'analitico.txt', multi_rows=True)
 
-try:
-    os.mkdir(DIRECTORIO_TXTS)
-    print('Se crea el directorio...')
-except:
-    print('El directorio ya estaba creado...')
+    # Zipeo la carpeta resultante en el directorio definido
+    import shutil
+    shutil.make_archive(DIRECTORIO_TXTS, 'zip', DIRECTORIO_TXTS)
 
-# Se genera la conexión
-conn = psycopg2.connect(host="localhost", port = 5432, database="exportaciones_unlu", user="postgres", password="888888")
+    # Cierro la conexión con PostgreSQL
+    conn.close()
 
-# Se transcriben los querys con los parámetros
-query_academicos = query_ministerio_academicos(LEGAJO, TITULO_ARAUCANO, SEDE_ARAUCANO, PROMEDIO_APLAZOS, PROMEDIO_SIN_APLAZOS)
-query_personales = query_ministerio_personales(LEGAJO)
-query_analitico = query_ministerio_analitico(LEGAJO)
+    # Se devuelve cadena de finalización del proceso
+    return 'El proceso de generación de los archivos zip ha finalizado'
+   
 
-generar_textfile(conn, query_academicos, DIRECTORIO_TXTS+'academicos.txt')
-generar_textfile(conn, query_personales, DIRECTORIO_TXTS+'personales.txt')
-generar_textfile(conn, query_analitico, DIRECTORIO_TXTS+'analitico.txt', multi_rows=True)
+if __name__ == '__main__':
 
-# Zipeo la nota resultante
-import shutil
-shutil.make_archive(DIRECTORIO_TXTS, 'zip', DIRECTORIO_TXTS)
-
-# Cierro la conexión
-conn.close()
+    # Importo las librerías para la renderización
+    from pywebio.input import input, input_group, NUMBER, FLOAT   
+    from pywebio.output import put_text
+    
+    # Se define el directorio de creación por defecto          
+    DIRECTORIO = 'C:/Users/Juan/Desktop/'
+    
+    # Se define el formulario de ingreso de datos
+    data = input_group("Generación de información para legalizaciones ante el Ministerio",[
+      input('Nombre de Carpeta de Salida (Convención: Apellido estudiante)', name='folder', required=True),
+      input('Legajo del estudiante', name='legajo', type=NUMBER, required=True),
+      input('Código de Tí­tulo Araucano', name='titulo_araucano', type=NUMBER, required=True),
+      input('Código de Sede Araucano', name='sede_araucano', type=NUMBER, required=True),
+      input('Promedio con aplazos', name='promedio_con_aplazos', type=FLOAT, required=True),
+      input('Promedio sin aplazos', name='promedio_sin_aplazos', type=FLOAT, required=True),
+      input('Directorio de trabajo', name='DIRECTORIO', required=True, value=DIRECTORIO),
+    ])
+    
+    # Llamo a la función principal de procesamiento de datos
+    result = main(data['DIRECTORIO'], data['folder'], data['legajo'], data['titulo_araucano'], data['sede_araucano'], data['promedio_con_aplazos'], data['promedio_sin_aplazos'])
+    
+    # Se muestra el resultado
+    put_text(result).style('color: red')
